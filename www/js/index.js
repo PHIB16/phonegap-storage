464a82fd21cae57e938bfb24c8078e10012f4e01
PHIB16 <phib1_16@uni.worc.ac.uk>
2017-10-16 15:12:53 +0100

    bvcx

--- www/js/index.js
+++ www/js/index.js
@@ -1,20 +1,13 @@
 
 window.localStorage.setItem("age", "19");
 
-window.localStorage.setItem(
-"email", 
- "cuck@cuckmail.com"
-);
+window.localStorage.setItem("email", "cuck@cuckmail.com");
 
-window.localStorage.setItem(
-"Name", 
- "Ronnie McDonnie"
+window.localStorage.setItem("Name",  "Ronnie McDonnie");
 
-);
-
-var age = window.localStorage.getItem("age");
-var email = window.localStorage.getItem("email");
-var Name = window.localStorage.getItem("Name")
+var age = localStorage.getItem("age");
+var email = localStorage.getItem("email");
+var Name = localStorage.getItem("Name")
 
 
 	$("#age").text(age);
