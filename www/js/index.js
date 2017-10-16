cafcd52ea7ab6b7f61b652cb8376956bcc91a5f1
PHIB16 <phib1_16@uni.worc.ac.uk>
2017-10-16 14:15:06 +0100

    1st

--- www/js/index.js
+++ www/js/index.js
@@ -2,45 +2,23 @@ var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
 //EVENT LISTENER
-document.addEventListener("deviceready", onDeviceReady, false);
-		
-	
-function updateDisplay() {
-	$("#launched").text("Application launched: " + launched_count);
-	$("#resumed").text("Application paused: " + paused_count);
-	$("#paused").text("Application resumed: " + resumed_count);
-}
 
+window.localStorage.setItem(
+"age", 
+ "19"
+);
 
-// device APIs are available
-//
+window.localStorage.setItem(
+"email", 
+ "cuck@cuckmail.com"
+);
 
-//EVENT HANDLER
-    function onDeviceReady() {
-	
-	document.addEventListener("resume", onResume, false);
-	document.addEventListener("pause", onPause, false);
-	
-	launched_count++;
-	updateDisplay();
-	    
-	alert("device ready");
-    }
+window.localStorage.setItem(
+"Name", 
+ "Ronnie McDonnie"
 
-//EVENT HANDLER
-    function onPause() {
-	
-	paused_count++;
-	updateDisplay();
-	    
-	alert("pause");
-    }
-	
-//EVENT HANDLER
-    function onResume() {
-		
-	resumed_count++;
-	updateDisplay();
-	    
-	alert("resume");
-    }
+);
+
+var test = window.localStorage.GetItem("Name");
+
+alert($test);
